c8267845d5549904807b20a8a4140c3c3b3e0178
DmitriiLevchenko <lev11081989@gmail.com>
2024-10-14 16:35:19 +1000

    01

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,11 @@
 // code
+let number1 = Math.floor(Math.random() * 100)
+console.log(number1);
+
+​let number = 1
+console.log(number++); 
+console.log(++number); 
+console.log(number--); 
+console.log(--number); 
+
+
