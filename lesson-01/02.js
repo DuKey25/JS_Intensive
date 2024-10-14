9fb57ecd7cc2a1dc48e79cd84c0d704d34205612
DmitriiLevchenko <lev11081989@gmail.com>
2024-10-14 18:56:27 +1000

    01

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,5 +1,5 @@
 // code
 let number1 = Math.floor(Math.random() * 100)
 
-console.log(number1%2 === 0 ? `${number1} :Это четное число` : `${number1} :Это нечетное число `);
+console.log(number1 % 2 === 0 ? `${number1} :Это четное число` : `${number1} :Это нечетное число `);
 
